ea2c30b9b559d3437a3cfe69367b107b0b451fa8
Jaume <jasato89@gmail.com>
2021-05-05 18:12:02 +0200

    added loop example

--- src/com/ironhack/labs/jasato/oop/Main.java
+++ src/com/ironhack/labs/jasato/oop/Main.java
@@ -1,10 +1,20 @@
 package com.ironhack.labs.jasato.oop;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println();
+        //Some common mistakes
+
+        int[] numbers = new int[] {1,2,3,4,5};
+
+
+        for (int i = 0; i <= numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
 
     }
 }
